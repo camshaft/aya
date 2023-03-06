789fc792cfb555ef0ddd586e7c4b37601fad81fa
Tuetuopay <tuetuopay@me.com>
2023-03-06 18:31:58 +0100

    test: add the possibility to run a test inside a network namespace
    For tests that do networking operations, this allows to have a
    clean-state network namespace and interfaces for each test. Mainly, this
    avoids "device or resource busy" errors when reusing the loopback
    interface across tests.

--- test/integration-test-macros/src/lib.rs
+++ test/integration-test-macros/src/lib.rs
@@ -4,10 +4,44 @@ use quote::quote;
 use syn::{parse_macro_input, Ident, ItemFn};
 
 #[proc_macro_attribute]
-pub fn integration_test(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn integration_test(attr: TokenStream, item: TokenStream) -> TokenStream {
     let item = parse_macro_input!(item as ItemFn);
-    let name = &item.sig.ident;
-    let name_str = &item.sig.ident.to_string();
+
+    let ItemFn {
+        attrs, vis, sig, ..
+    } = &item;
+    let name = &sig.ident;
+    let name_str = &sig.ident.to_string();
+
+    // Wrap in a netns exec
+    let mut netns = false;
+    let parser = syn::meta::parser(|meta| {
+        if meta.path.is_ident("netns") {
+            netns = true
+        }
+        Ok(())
+    });
+    parse_macro_input!(attr with parser);
+
+    let item = if netns {
+        // A vec cannot be directly expanded, and an empty #[] yields errors...
+        let attrs = if attrs.is_empty() {
+            quote!()
+        } else {
+            quote!(#[#(#attrs),*])
+        };
+        quote! {
+            #attrs
+            #vis #sig {
+                #item
+                let netns = crate::utils::Netns::new();
+                netns.exec(|| #name());
+            }
+        }
+    } else {
+        quote!(#item)
+    };
+
     let expanded = quote! {
         #item
 
